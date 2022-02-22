efad0d780f3742155d607048cb7fe55fcc674980
henry521 <henriquep.freitas@hotmail.com>
2022-02-21 23:34:22 -0300

    Novo método adicionar botões
    Adicionar botões com o método do GridBagLayout

--- Calculadora/src/visao/Botao.java
+++ Calculadora/src/visao/Botao.java
@@ -1,14 +1,25 @@
 package visao;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Insets;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton {
 	
-	public Botao() {
-		setBackground(Color.GREEN);
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setOpaque(true);
+		setBackground(cor);
+		setFont(new Font("consolas", Font.PLAIN, 20));
+		setForeground(Color.WHITE);
+		setMargin(new Insets(0, 0, 0, 0));
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	}
+	
+	
 
 }
