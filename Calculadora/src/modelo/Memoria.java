f05c5fb3448d949d9a0f3de4395cff48ae175bb6
henry521 <henriquep.freitas@hotmail.com>
2022-03-02 15:09:45 -0300

    Lógica Calculadora
    Aplicada a lógica que vai determinar qual o tipo de botão que foi acionado, e qual é a função escolhida...

--- Calculadora/src/modelo/Memoria.java
+++ Calculadora/src/modelo/Memoria.java
@@ -3,49 +3,85 @@ package modelo;
 import java.util.List;
 import java.util.ArrayList;
 
-
-
 public class Memoria {
-	
+
+	private enum TipoComando {
+		ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA;
+	}
+
 	private static final Memoria instancia = new Memoria();
-	
-	private final List<MemoriaObservador> observadores = 
-			new ArrayList<>();
-	
+
+	private final List<MemoriaObservador> observadores = new ArrayList<>();
+
 	private String textoAtual = "";
-	
+
 	private Memoria() {
-		
+
 	}
 
 	public static Memoria getInstancia() {
 		return instancia;
 	}
-	
+
 	public void adicionarObservador(MemoriaObservador observador) {
 		observadores.add(observador);
 	}
-	
-	public String getTextoAtual() { 
-		
+
+	public String getTextoAtual() {
+
 		// Se o texto atual for vazio, retorna 0
 		// Caso contrario, retorna texto atual.
 		return textoAtual.isEmpty() ? "0" : textoAtual;
 	}
-	
-	public void processarComando(String valor) { 
-		
-		if("AC".equals(valor)) {
+
+	public void processarComando(String texto) {
+
+		TipoComando tipoComando = detectarTipoComando(texto);
+		System.out.println(tipoComando);
+
+		if ("AC".equals(texto)) {
 			textoAtual = "0";
 		} else {
-			textoAtual += valor;
+			textoAtual += texto;
 		}
-		
-		textoAtual += valor;
-		
+
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
 	}
-	
-	
+
+	private TipoComando detectarTipoComando(String texto) {
+
+		if (textoAtual.isEmpty() && texto == "0") {
+			return null;
+		}
+
+		// verificar se o texto digitado pode ou não
+		// ser convertido para um valor inteiro
+
+		try {
+			Integer.parseInt(texto);
+			return TipoComando.NUMERO;
+		} catch (NumberFormatException e) {
+			// Quando não for número:
+
+			if ("AC".equals(texto)) {
+				return TipoComando.ZERAR;
+			} else if ("/".equals(texto)) {
+				return TipoComando.DIV;
+			} else if ("*".equals(texto)) {
+				return TipoComando.MULT;
+			} else if ("+".equals(texto)) {
+				return TipoComando.SOMA;
+			} else if ("-".equals(texto)) {
+				return TipoComando.SUB;
+			} else if ("=".equals(texto)) {
+				return TipoComando.IGUAL;
+			} else if (",".equals(texto)) {
+				return TipoComando.VIRGULA;
+			} 
+
+		}
+
+		return null;
+	}
 
 }
